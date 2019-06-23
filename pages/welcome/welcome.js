1bd1837217e304fa2b35c2f8fd05241ab2e93ae1
ZWJ <330564052@qq.com>
2019-06-23 21:27:08 +0800

    初始提交

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,24 @@
+// pages/welcome/welcome.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  onStory:function(event){
+    wx.navigateTo({
+      url: '../story/story-list/story-list',
+    })
+  }
+
+})
